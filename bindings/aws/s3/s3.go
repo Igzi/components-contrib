6087af1b13a0443abc77516de27464c4668aa8e2
Igor Pavlovic (E-Search) <a-ipavlovic@microsoft.com>
2021-09-13 13:09:13 +0200

    Backup from s3

--- bindings/aws/s3/s3.go
+++ bindings/aws/s3/s3.go
@@ -7,6 +7,7 @@ package s3
 
 import (
 	"bytes"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"strconv"
@@ -26,6 +27,8 @@ const (
 	metadataKeyBlobName = "blobName"
 	metadataKeyOffset = "offset"
 	metadataKeyCount = "count"
+	metadataKeyData = "data"
+	MinBufferSize = 5242880
 )
 
 // AWSS3 is a binding for an AWS S3 storage bucket
@@ -35,6 +38,11 @@ type AWSS3 struct {
 	uploader *s3manager.Uploader
 	downloader *s3manager.Downloader
 	logger   logger.Logger
+	PartNum int
+	MultipartResponse *s3.CreateMultipartUploadOutput
+	CompletedParts []*s3.CompletedPart
+	buffer []byte
+	S3 *s3.S3
 }
 
 type s3Metadata struct {
@@ -62,6 +70,9 @@ func (s *AWSS3) Init(metadata bindings.Metadata) error {
 		return err
 	}
 	s.metadata = m
+	s.PartNum = 1
+	s.MultipartResponse = nil
+	s.buffer = nil
 
 	return nil
 }
@@ -70,7 +81,10 @@ func (s *AWSS3) Operations() []bindings.OperationKind {
 	return []bindings.OperationKind{
 		bindings.CreateOperation,
 		bindings.GetOperation,
+		bindings.DeleteOperation,
 		"head",
+		"put",
+		"putblocklist",
 	}
 }
 
@@ -81,8 +95,14 @@ func (s *AWSS3) Invoke(req *bindings.InvokeRequest) (*bindings.InvokeResponse, e
 		return s.create(req)
 	case bindings.GetOperation:
 		return s.get(req)
+	case bindings.DeleteOperation:
+		return s.delete(req)
 	case "head":
 		return s.head(req)
+	case "put":
+		return s.put(req)
+	case "putblocklist":
+		return s.putblocklist(req)
 	default:
 		return nil, fmt.Errorf("unsupported operation %s", req.Operation)
 	}
@@ -105,6 +125,25 @@ func (s *AWSS3) create(req *bindings.InvokeRequest) (*bindings.InvokeResponse, e
 	}
 
 	_, err := s.uploader.Upload(&requestInput)
+
+	return nil, err
+}
+
+func (s *AWSS3) delete(req *bindings.InvokeRequest) (*bindings.InvokeResponse, error) {
+	key := ""
+	if val, ok := req.Metadata[metadataKeyBlobName]; ok && val != "" {
+		key = val
+	} else {
+		key = uuid.New().String()
+		s.logger.Debugf("key not found. generating key %s", key)
+	}
+
+	requestInput := s3.DeleteObjectInput{
+		Bucket: aws.String(s.metadata.Bucket),
+		Key:    aws.String(key),
+	}
+
+	_, err := s.S3.DeleteObject(&requestInput)
 
 	return nil, err
 }
@@ -148,8 +187,8 @@ func (s *AWSS3) head(req *bindings.InvokeRequest) (*bindings.InvokeResponse, err
 		Key:    aws.String(req.Metadata[metadataKeyBlobName]),
 	}
 
-	svc := s3.New(s.session)
-	result, _ := svc.HeadObject(&requestInput)
+
+	result, err := s.S3.HeadObject(&requestInput)
 
 	metadata := make(map[string]string)
 	metadata["Content-Length"] = strconv.FormatInt(aws.Int64Value(result.ContentLength), 10)
@@ -157,7 +196,108 @@ func (s *AWSS3) head(req *bindings.InvokeRequest) (*bindings.InvokeResponse, err
 	return &bindings.InvokeResponse{
 	Data:     nil,
 	Metadata: metadata,
-	}, nil
+	}, err
+}
+
+func (s *AWSS3) put(req *bindings.InvokeRequest) (*bindings.InvokeResponse, error) {
+	buffer, _ := hex.DecodeString(req.Metadata[metadataKeyData])
+
+	if(s.MultipartResponse == nil) {
+		s.MultipartResponse, _ = s.S3.CreateMultipartUpload(&s3.CreateMultipartUploadInput{
+			Bucket: aws.String(s.metadata.Bucket),
+			Key:    aws.String(req.Metadata[metadataKeyBlobName]),
+		})
+	}
+
+	if(s.buffer == nil){
+		s.buffer = buffer
+	} else{
+		s.buffer = append(s.buffer,buffer...)
+	}
+
+	if(len(s.buffer) < MinBufferSize){
+		return nil,nil
+	}
+
+	uploadResp, err := s.S3.UploadPart(&s3.UploadPartInput{
+		Body:          bytes.NewReader(s.buffer),
+		Bucket:        s.MultipartResponse.Bucket,
+		Key:           s.MultipartResponse.Key,
+		PartNumber:    aws.Int64(int64(s.PartNum)),
+		UploadId:      s.MultipartResponse.UploadId,
+		ContentLength: aws.Int64(int64(len(s.buffer))),
+	})
+
+	s.buffer = nil
+
+	if err != nil {
+		s.S3.AbortMultipartUpload(&s3.AbortMultipartUploadInput{
+			Bucket:   s.MultipartResponse.Bucket,
+			Key:      s.MultipartResponse.Key,
+			UploadId: s.MultipartResponse.UploadId,
+		})
+		s.PartNum = 1
+		s.MultipartResponse = nil
+		s.CompletedParts = nil
+	} else{
+		s.CompletedParts = append(s.CompletedParts, &s3.CompletedPart{
+			ETag:       uploadResp.ETag,
+			PartNumber: aws.Int64(int64(s.PartNum)),
+		})
+		s.PartNum++
+	}
+
+	return nil, err
+}
+
+func (s *AWSS3) putblocklist(req *bindings.InvokeRequest) (*bindings.InvokeResponse, error) {
+	if(s.MultipartResponse == nil) {
+		return nil, nil
+	}
+
+	uploadResp, err := s.S3.UploadPart(&s3.UploadPartInput{
+		Body:          bytes.NewReader(s.buffer),
+		Bucket:        s.MultipartResponse.Bucket,
+		Key:           s.MultipartResponse.Key,
+		PartNumber:    aws.Int64(int64(s.PartNum)),
+		UploadId:      s.MultipartResponse.UploadId,
+		ContentLength: aws.Int64(int64(len(s.buffer))),
+	})
+
+	s.buffer = nil
+
+	if err != nil {
+		s.S3.AbortMultipartUpload(&s3.AbortMultipartUploadInput{
+			Bucket:   s.MultipartResponse.Bucket,
+			Key:      s.MultipartResponse.Key,
+			UploadId: s.MultipartResponse.UploadId,
+		})
+		s.PartNum = 1
+		s.MultipartResponse = nil
+		s.CompletedParts = nil
+
+		return nil,err
+	} else{
+		s.CompletedParts = append(s.CompletedParts, &s3.CompletedPart{
+			ETag:       uploadResp.ETag,
+			PartNumber: aws.Int64(int64(s.PartNum)),
+		})
+		s.PartNum++
+	}
+
+	_,err = s.S3.CompleteMultipartUpload(&s3.CompleteMultipartUploadInput{
+		Bucket:   s.MultipartResponse.Bucket,
+		Key:      s.MultipartResponse.Key,
+		UploadId: s.MultipartResponse.UploadId,
+		MultipartUpload: &s3.CompletedMultipartUpload{
+			Parts: s.CompletedParts,
+		},
+	})
+
+	s.MultipartResponse = nil
+	s.CompletedParts = nil
+	s.PartNum = 1
+	return nil, err
 }
 
 func (s *AWSS3) parseMetadata(metadata bindings.Metadata) (*s3Metadata, error) {
@@ -180,8 +320,8 @@ func (s *AWSS3) getClient(metadata *s3Metadata) (error) {
 	if err != nil {
 		return err
 	}
-	
-	s.session = sess
+
+	s.S3 = s3.New(sess)
 	s.uploader = s3manager.NewUploader(sess)
 	s.downloader = s3manager.NewDownloader(sess)
 
